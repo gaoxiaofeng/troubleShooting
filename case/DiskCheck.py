9c01828cefc64b1eb62fb50a552499ae7cfc8a0c
jargao <jarvan.gao@nokia.com>
2017-08-07 13:56:16 +0800

    enhance

---
+++ case/DiskCheck.py
@@ -0,0 +1,11 @@
+from _BaseCase import _BaseCase
+class DiskCheck(_BaseCase):
+    """
+    To Check NBI3GC node disk usage.
+    """
+    def __init__(self):
+        super(DiskCheck,self).__init__()
+        self.passCondition = "{DiskSizeCheck} and {DiskInodesCheck}"
+
+
+
--- case/NamingServiceCheck.py
+++ case/NamingServiceCheck.py
@@ -1,7 +1,7 @@
 from _BaseCase import _BaseCase
 class NamingServiceCheck(_BaseCase):
     """
-    To Check NBI3GC Function with IIOP Security.
+    To Check IOR registed in NamingServer.
     """
     def __init__(self):
         super(NamingServiceCheck,self).__init__()
