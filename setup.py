9bc2383f7da94baf259cb420190cf6f5b5577654
jarvan <zeus.gao@foxmail.com>
2017-09-09 00:21:48 +0800

    enhance for pip

--- setup.py
+++ setup.py
@@ -2,14 +2,39 @@ from setuptools import setup, find_packages
 import sys
 from os.path import dirname,abspath,join
 import os
-
+from os.path import abspath, join, dirname
+from subprocess import list2cmdline
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
 CURDIR = dirname(abspath(__file__))
 SRC = join(CURDIR,"src")
 sys.path.append(SRC)
-SCRIPTS = [join(SRC,"bin"),join(SRC,"bin","pyts")]
+SCRIPTS = [join(SRC,"bin","pyts")]
 WINDOWS = os.sep == '\\'
 if WINDOWS:
     SCRIPTS.append(join(SRC,"bin","pyts.bat"))
+class custom_install_scripts(install_scripts):
+
+    def run(self):
+        install_scripts.run(self)
+        if WINDOWS:
+            self._replace_interpreter_in_bat_files()
+
+    def _replace_interpreter_in_bat_files(self):
+        print "replacing interpreter in robot.bat and rebot.bat."
+        interpreter = list2cmdline([sys.executable])
+        for path in self.get_outputs():
+            if path.endswith('pyts.bat'):
+                with open(path, 'r') as input:
+                    replaced = input.read().replace('python', interpreter)
+                with open(path, 'w') as output:
+                    output.write(replaced)
+
+with open("README.rst","rb") as f:
+    DES = f.read()
+
+
+
 # print find_packages(where=SRC)
 # sys.exit()
 with open(join(CURDIR,"src","troubleshooting","framework","version","version.py"),"rb") as f:
@@ -38,4 +63,6 @@ setup(
     keywords="troubleshooting gaoxiaofeng",
     url="https://github.com/gaoxiaofeng/troubleShooting",  # project home page, if any
     # could also include long_description, download_url, classifiers, etc.
+    cmdclass     = {'install_scripts': custom_install_scripts},
+    long_description=DES,
 )
